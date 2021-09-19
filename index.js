94a35345e6623b0a8cc64d048b5243e29d8fcff1
Shivammehra2001 <85388907+Shivammehra2001@users.noreply.github.com>
2021-09-19 13:27:51 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,111 @@
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
+
+#img11{
+    margin: auto;
+    display: block;
+    
+    width: 50%;
+    height: 50%;
+}
+
+#img10{
+    margin: auto;
+    width: 50%;
+    height: 50%;
+}
